415fef120906fa5ea44e60e4278e2eab66af16d0
Ian A. Cook <nai888@gmail.com>
2017-11-04 18:16:26 -0500

    Add remaining components
    Added components for the text entry, the control buttons, the options
    panel, and the preview screen.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component'
+import { FooterComponent } from './footer/footer.component';
+import { EntryComponent } from './entry/entry.component';
+import { OptionsComponent } from './options/options.component';
+import { ControlsComponent } from './controls/controls.component';
+import { PreviewComponent } from './preview/preview.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    EntryComponent,
+    OptionsComponent,
+    ControlsComponent,
+    PreviewComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/controls/controls.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-controls',
+  templateUrl: './controls.component.html',
+  styleUrls: ['./controls.component.sass']
+})
+export class ControlsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
